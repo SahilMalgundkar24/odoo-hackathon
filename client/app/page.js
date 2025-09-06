a5a803eb2490d8d8463b06d5f59a98f633505a1e
SahilMalgundkar24 <sahilmalgundkar321@gmail.com>
2025-09-06 13:52:53 +0530

    Fixed the navbar and the hero section

--- client/app/page.js
+++ client/app/page.js
@@ -2,22 +2,28 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Products from "@/components/Product";
+import { FiArrowRight } from "react-icons/fi";
 
 export default function Home() {
   const router = useRouter();
   return (
     <>
       <div className="w-full h-full">
-        <Navbar />
-        <Hero />
+        <div className="px-16 py-3">
+          <Navbar />
+          <Hero />
+        </div>
         <Products />
       </div>
-      <div className="absolute bottom-7 right-7">
+      <div className="fixed bottom-7 right-7 z-50">
         <div
           onClick={() => router.push("/seller")}
-          className="border border-gray-400 p-4 text-sm rounded-full flex items-center justify-center cursor-pointer"
+          className="border border-gray-400 py-4 px-7 gap-3 text-sm rounded-full flex items-center justify-center bg-white cursor-pointer"
         >
           Sell your stuff
+          <FiArrowRight />
         </div>
       </div>
     </>
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -4,38 +4,50 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import CartModal from "./CartModal";
 
 const Navbar = () => {
-	return (
-		<div className="flex items-center justify-between">
-			<div className="w-1/3">
-				<div>
-					<img src="/images/logo.png" />
-				</div>
-			</div>
-			<div className="w-1/3 flex justify-center items-center gap-7">
-				<h1 className="text-sm">Home</h1>
-				<h1 className="text-sm">Explore</h1>
-				<h1 className="text-sm">Rental Machines</h1>
-				<h1 className="text-sm">Workshops</h1>
-			</div>
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
-			<div className="w-1/3 flex items-center justify-end">
-				<div className="flex items-center bg-[#F7F7F7] px-7 rounded-full">
-					<input
-						type="text"
-						placeholder="Search"
-						className="py-2 rounded-full placeholder-[#414141] focus:outline-none"
-						suppressHydrationWarning
-					/>
-					<span className=" text-gray-500 pointer-events-none ml-2">
-						<FiSearch color="#9B9B9B" />
-					</span>
-				</div>
-				<div>
-					<MdOutlineShoppingCart size={30} className="ml-7" color="#909090" />
-				</div>
-			</div>
-		</div>
-	);
+  const toggleCart = () => {
+    setIsCartOpen(!isCartOpen);
+  };
+  return (
+    <div className="flex items-center justify-between pb-3">
+      <div className="w-1/3">
+        <div>
+          <img src="/images/logo.png" />
+        </div>
+      </div>
+      <div className="w-1/3 flex justify-center items-center gap-7">
+        <h1 className="text-sm">Home</h1>
+        <h1 className="text-sm">Explore</h1>
+        <h1 className="text-sm">Rental Machines</h1>
+        <h1 className="text-sm">Workshops</h1>
+      </div>
+
+      <div className="w-1/3 flex items-center justify-end">
+        <div className="flex items-center bg-[#F7F7F7] px-7 rounded-full">
+          <input
+            type="text"
+            placeholder="Search"
+            className="py-2 rounded-full placeholder-[#414141] focus:outline-none"
+            suppressHydrationWarning
+          />
+          <span className=" text-gray-500 pointer-events-none ml-2">
+            <FiSearch color="#9B9B9B" />
+          </span>
+        </div>
+        <div>
+          <button
+            suppressHydrationWarning
+            onClick={toggleCart}
+            className="ml-7 hover:opacity-70 transition-opacity cursor-pointer"
+          >
+            <MdOutlineShoppingCart size={30} color="#909090" />
+          </button>
+        </div>
+      </div>
+      <CartModal isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
+    </div>
+  );
 };
 
 export default Navbar;
